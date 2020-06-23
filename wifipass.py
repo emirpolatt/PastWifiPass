95725e92499e5b485fab2241920f09f990220aa5
espday <alganbarlas@outlook.com>
2020-06-23 13:00:31 +0300

    Add comments lines, some fixes

--- wifipass.py
+++ wifipass.py
@@ -11,7 +11,6 @@ Date: 22.06.2020
 """
 
 import os, sys, time
-import subprocess
 import pyfiglet
 
 assert ('linux' in sys.platform), "This script runs only Linux Systems"
@@ -25,19 +24,22 @@ class tColors:
     FAIL = '\033[91m'
     BOLD = '\033[1m'
 
+if not os.getuid() == 0:
+    sys.exit(tColors.FAIL + "Wifipass has to be run with root")
+
 def HelloMessage():
-    helloMessage = "PastWIFIPass"
+    result = tColors.OKBLUE + pyfiglet.figlet_format("PastWIFIPass")
+    print(result)
+
     description = tColors.OKGREEN + """
-    This script finds your wifi passwords saved in the past(Only Linux).
+    This script finds your wifi passwords saved in the past(Only Linux, V1).
     
     For use: You just have to give the 'Run' command.
     
-    For Quit: Give the 'exit' command.
+    For quit: Give the 'exit' command.
     
     Author: Emir Polat (@espday)
     """
-    result = tColors.OKBLUE + pyfiglet.figlet_format(helloMessage)
-    print(result)
     print(description)
 
 HelloMessage()
@@ -45,6 +47,10 @@ HelloMessage()
 detectOs = os.name
 
 def LinuxSystems():
+    # This is linux commands for the find of past wifi passwords.
+    # Normally the files where wifi passwords are kept on Linux end with ".nmconnection".
+    # With the sed command and the parameters we give afterwards, we delete this ".nmconnection" extension and show it to the user.
+    # For example: wifi.nmconnection to -> Only: wifi
     allNetworks = "cd /etc/NetworkManager/system-connections/ && ls -1 | sed 's/\.[a-z]*$//'"
     command = os.popen(allNetworks)
     readCommand = command.read()
@@ -53,9 +59,18 @@ def LinuxSystems():
 
     selectWifi = input(tColors.OKBLUE + "Which Network ? \n" + tColors.OKBLUE + "\n>>> ")
 
+    if selectWifi == 'exit':
+        print("Good bye")
+        time.sleep(0.5)
+        sys.exit()
+
+    # We check if there is a file with the entered value -> selecWifi
     checkFile = os.path.exists("/etc/NetworkManager/system-connections/" + selectWifi + ".nmconnection")
 
     if checkFile == True:
+        #  If there is such a file; By taking root permissions, we go to the required directory,
+        #  delete the 'psk =' value where the password is kept,
+        #  and by calling only the 16th line of the file with many lines, we only get the password.
         password = "sudo sed 's/^psk=//' /etc/NetworkManager/system-connections/" + selectWifi + ".nmconnection" + " | sudo sed '16!d' "
         passwordCommand = os.popen(password)
         readPass = passwordCommand.read()
@@ -65,22 +80,19 @@ def LinuxSystems():
     else:
         print(tColors.FAIL + "\n <<<<< No such file! Try again by typing 'run' >>>>>")
 
-
-
 while True:
     runCommand = input(tColors.HEADER + ">> ")
 
     if runCommand == "Run" or runCommand == "run":
         LinuxSystems()
 
-
-    elif runCommand != "Run" and runCommand != "run" and runCommand != 'exit':
-        print("You have logged in incorrectly. Please enter run command only")
-
     elif runCommand == 'exit':
         print("Good bye!")
-        time.sleep(1)
+        time.sleep(0.5)
         sys.exit()
+
+    else:
+        print("You have logged in incorrectly. Please enter run command only")
 
 # Linux and Mac systems == 'posix', Windows == 'nt'
 if detectOs == 'posix':
